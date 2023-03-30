b7475f8bf30b860a14ecfe623708b927896a0c25
Lucas Boemeke <lucas@blueshift.cc>
2023-03-30 19:40:45 -0300

    totalCards bohr

--- api/core/index.ts
+++ api/core/index.ts
@@ -1,12 +1,37 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
-import createAPI from 'lambda-api';
+import { APIGatewayEvent, Context } from "aws-lambda";
+import createAPI from "lambda-api";
 
 const api = createAPI();
 
-api.get('/', async (req, res) => {
-    return { status: 'Hello from Function!!' };
+api.get("/", async (req, res) => {
+  const fetch = require("node-fetch");
+
+  const apiKey = process.env.TRELLO_API_KEY;
+  const apiToken = process.env.TRELLO_API_TOKEN;
+  const boardId = "8nuhV3Sx";
+
+  const getCardCount = async () => {
+    try {
+      const response = await fetch(`https://api.trello.com/1/boards/${boardId}/cards?filter=all&key=${apiKey}&token=${apiToken}`);
+
+      //console.log(response);
+
+      if (!response.ok) {
+        throw new Error("Erro ao buscar os cards do Trello");
+      }
+
+      const cards = await response.json();
+      return cards.length;
+    } catch (error) {
+      console.error("Erro:", error.message);
+    }
+  };
+
+  const cardCount = await getCardCount();
+
+  return { totalCards: cardCount };
 });
 
 export async function handler(event: APIGatewayEvent, context: Context) {
-    return await api.run(event, context);
-};
+  return await api.run(event, context);
+}
